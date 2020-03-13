eaf73fcd4bd08c9c53db609e7d5703270b1c6c7a
user <user@DESKTOP-GNIE0O4>
2020-03-13 17:06:52 +0530

    commit

---
+++ Prototype/src/Demo.java
@@ -0,0 +1,11 @@
+class Demo 
+{ 
+    public static void main (String[] args) 
+    { 
+        ColorType.getColor("blue").addColor(); 
+        ColorType.getColor("purple").addColor(); 
+        ColorType.getColor("purple").addColor(); 
+        ColorType.getColor("blue").addColor(); 
+    } 
+} 
+
